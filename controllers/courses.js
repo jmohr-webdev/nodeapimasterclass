66a58f8ef9ec900f509e13e409221daa050ad819
J. Mohr <pixels.and.glitches@gmail.com>
2021-05-11 10:55:37 -0500

    Added advanced request methods, pagination, and Course model

---
+++ controllers/courses.js
@@ -0,0 +1,3 @@
+const Course = require('../models/Course');
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
---
+++ routes/couses.js
@@ -0,0 +1,3 @@
+// @desc      Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    public
