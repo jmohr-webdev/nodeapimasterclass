e375e1a010045b0dd2352e6543cbfb4e63426fa5
J. Mohr <pixels.and.glitches@gmail.com>
2021-05-17 13:28:42 -0500

    Added advanced querying middleware and refactored

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
+
 const {
   getCourses,
   getCourse,
@@ -9,7 +12,16 @@ const {
   deleteCourse,
 } = require('../controllers/courses');
 
-router.route('/').get(getCourses).post(addCourse);
+router
+  .route('/')
+  .get(
+    advancedResults(Course, {
+      path: 'bootcamp',
+      select: 'name, description',
+    }),
+    getCourses
+  )
+  .post(addCourse);
 router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
