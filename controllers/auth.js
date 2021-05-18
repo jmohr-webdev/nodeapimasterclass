ba9b7b4bec9676603d5eac470332a3e5267b921f
J. Mohr <pixels.and.glitches@gmail.com>
2021-05-18 08:58:54 -0500

    Added login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,3 +16,31 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, token });
 });
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if Password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
